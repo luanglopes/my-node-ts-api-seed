51599d05f8362a8b118989938c9a48a4ddbce656
Luan Lopes <luanglopes@gmail.com>
2020-07-17 22:58:15 -0300

    register profile routes

--- src/modules/users/infra/http/routes/index.ts
+++ src/modules/users/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import usersRouter from './users.router'
 import authRouter from './auth.router'
+import profileRouter from './profile.router'
 
 const router = Router()
 
 router.use('/users', usersRouter)
 router.use('/auth', authRouter)
+router.use('/profile', profileRouter)
 
 export default router
