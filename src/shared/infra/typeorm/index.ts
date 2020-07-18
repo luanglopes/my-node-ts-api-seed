76a8fda0b749b5b3cc7168102c69d819e0567958
Luan Lopes <luanglopes@gmail.com>
2020-07-17 23:12:39 -0300

    remove cross-env dependency

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,16 +1,18 @@
 import { createConnection, getConnectionOptions, EntitySchema } from 'typeorm'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-const adjustPathsToBuild = (path: string | Function | EntitySchema): string =>
+const adjustPathsAndExtensionsToBuild = (path: string | Function | EntitySchema): string =>
   path.toString().replace('src', 'dist').replace('.ts', '.js')
 
 const initDatabase = async (): Promise<void> => {
   const connOptions = await getConnectionOptions()
 
-  if (process.env.NODE_ENV === 'production') {
+  const fileExtension = __filename.split('/').pop().split('.').pop()
+
+  if (fileExtension === 'js') {
     Object.assign(connOptions, {
-      entities: connOptions.entities?.map(adjustPathsToBuild),
-      migrations: connOptions.migrations?.map(adjustPathsToBuild),
+      entities: connOptions.entities?.map(adjustPathsAndExtensionsToBuild),
+      migrations: connOptions.migrations?.map(adjustPathsAndExtensionsToBuild),
     })
   }
 
